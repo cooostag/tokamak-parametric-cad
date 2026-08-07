7982f12753389bda55c7f4a97cc61317e02ebb1d
cooostag <costantino.galletti@outlook.de>
2026-08-07 22:26:48 +0200

    Working full Circle parametrized shape, started cylinder port, need to fix it though

--- Vacum_chamber_parametrized.py
+++ Vacum_chamber_parametrized.py
@@ -12,11 +12,10 @@ theta = np.linspace(0, 2 * np.pi, n_points, endpoint=False)  # closed curve, don
 # Reproducing the circle from the miller paper
 # https://www.cambridge.org/core/journals/journal-of-plasma-physics/article/simple-general-realistic-robust-analytic-tokamak-equilibria-part-1-limiter-and-divertor-tokamaks/0995989BC8BFB530B273A0666D0C0423
 
-#testing the cirlce
-
-epsilon=0.33
+#the triangularity values are e=0.4, kappa=2 delta=0.4 v=1
+epsilon=0.4
 kappa=1
-delta=0
+delta=0.75
 vau=1
 alfa=50
 R0=alfa/epsilon
@@ -24,26 +23,77 @@ delta_hat = np.arcsin(delta)
 R_s = R0 + alfa * np.cos(theta + delta_hat * np.sin(theta))
 Z_s = alfa * kappa * np.sin(theta)
 
-thickness = 4
+thickness = 4 # thickness of the wall
+R_internal = 150 # distance from revolution of the center of the vacuum's chamber profile
 
-points = list(zip(R_s.tolist(), Z_s.tolist()))
+points_outer = list(zip(R_s.tolist(), Z_s.tolist()))
+#list and zip to make a 1:1 matching (and organized in a list) between the 2 coordinates to identify the points and pass it as
 
-
+# Inner boundary: same Miller curve, smaller minor radius (alfa - thickness)
+alfa_inner = alfa - thickness
+R_s_inner = R0 + alfa_inner * np.cos(theta + delta_hat * np.sin(theta))
+Z_s_inner = alfa_inner * kappa * np.sin(theta)
+points_inner = list(zip(R_s_inner.tolist(), Z_s_inner.tolist()))
 
 #Cannot use offset to create a hollow circle because build123d is not able to offset a closed geometry
 #for this reason instead of using offset I will subtract 2 different profiles. This will be useful to
 #model a not constant thicknes
 
-with BuildSketch() as test_profile:
+with BuildSketch() as vessel_profile:
     with BuildLine():
-        Spline(*points, periodic=True)
-    make_face()
+        Spline(*points_outer, periodic=True)
+    outer_face= make_face()
 
-show(test_profile.sketch)
+with BuildSketch() as inner_sketch:
+    with BuildLine():
+        Spline(*points_inner, periodic=True)
+    inner_face = make_face()
 
-print ("Test")
+vessel_face = outer_face - inner_face  # or Face subtraction, check exact API
 
-#check cirlce geometry
+moved_vessel_face = vessel_face.moved(Location((R_internal, 0, 0)))
+
+show(moved_vessel_face)
+
+print ("sub part 1")
+
+
+with BuildPart() as VC_3D:
+    # Revolve the 2D sketch face around that axis
+    revolve(profiles=moved_vessel_face, axis=Axis.Y, revolution_arc=360)
+
+# Show the full 3D vessel!
+show(VC_3D)
+
+print ("Sub part 2")
+
+
+#check cirlce geometry -> Check succesfulll
 #residual = np.sqrt((R_s - R0)**2 + Z_s**2) - a
 #residual.max(), residual.min()
 
+#Next: Cut a cylinder
+#1 create a cylinder with build part
+#2 put it at the right place!
+#3 subtract the cylinder from the vacum chamber
+
+#1 create cylinder
+
+#test this
+R_port= 10
+height_port= 20
+
+#Create cylinder and put it at the right place direcly
+with BuildPart() as cylinder_base_port:
+    with Locations (R_internal,0,0):
+        Cylinder (radius=R_port, height=height_port)
+
+
+show (cylinder_base_port)
+#Simple addiction of parts like this is not possible
+#VC_withPort= VC_3D+cylinder_base_port
+
+#show  (VC_withPort)
+
+
+print ("Success")
