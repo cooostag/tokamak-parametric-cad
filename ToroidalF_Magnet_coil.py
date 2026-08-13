10aa236aba4b131f2a195b4d7bbdb04c7c348466
cooostag <costantino.galletti@outlook.de>
2026-08-13 20:00:36 +0200

    RUNNING simple toroidal magnetic coil with rectangular shape (small code polish)

--- ToroidalF_Magnet_coil.py
+++ ToroidalF_Magnet_coil.py
@@ -5,19 +5,16 @@ from build123d import *
 from ocp_vscode import show
 
 #coild_width, coil_height all need to be put in params file or in a 2nd TF coil param
-coil_width: float = 4.0
-coil_height: float = 4.0
-
+coil_width_: float = 4.0
+coil_height_: float = 4.0
 gap_ : float = 5.0
 # R = 125 + 50 + 5 + 5 = 185
 
-#calling the functions within the file, is it good practice?
-params_ = TokamakParams()
-coilR0 = coil_R0(params_)
-
 print ("stoP")
 
-def toroidal_coil (gap):
+def toroidal_coil (gap, coil_width,coil_height):
+    params_ = TokamakParams() #instiantate the class from params and calling the functions within the coil function, is it good practice?
+    coilR0 = coil_R0(params_)
     coil_Re = coilR0 + params_.alfa + params_.thickness + gap
 
 #Simplifcation: rectangular coil -> Improve to better geometry
@@ -31,7 +28,7 @@ def toroidal_coil (gap):
     return coil
 
 
-TF_coil = toroidal_coil(gap_)
+TF_coil = toroidal_coil(gap_, coil_width_, coil_height_)
 show(TF_coil) # remove the show, was done only for check practice
 
 print ("heyholetsgo")
